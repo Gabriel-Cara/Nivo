fac22f749aeb2590217436bd42791a4dbb11ff1f
Gabriel-Cara <gabricar28@gmail.com>
2024-02-22 15:51:10 -0300

    first step aula 02

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,29 @@
-import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react";
+import {
+  Plus,
+  Search,
+  Filter,
+  FileDown,
+  MoreHorizontal,
+  Loader2,
+} from "lucide-react";
 import { Header } from "./components/header";
 import { Tabs } from "./components/tabs";
 import { Button } from "./components/ui/button";
 import { Control, Input } from "./components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
 import { Pagination } from "./components/pagination";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { CreateTagForm } from "./components/create-tag-form";
 
 export interface TagResponse {
   first: number;
@@ -21,19 +37,24 @@ export interface TagResponse {
 
 export interface Tag {
   title: string;
+  slug: string;
   amountOfVideos: number;
   id: string;
 }
 
 export function App() {
-  const [searchParams, setSearchParams ] = useSearchParams();
-  const urlFilter = searchParams.get("filter") ?? ''
-  
+  const [searchParams, setSearchParams] = useSearchParams();
+  const urlFilter = searchParams.get("filter") ?? "";
+
   const [filter, setFilter] = useState(urlFilter);
-  
+
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
 
-  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+  const {
+    data: tagsResponse,
+    isLoading,
+    isFetching,
+  } = useQuery<TagResponse>({
     queryKey: ["get-tags", urlFilter, page],
     queryFn: async () => {
       const response = await fetch(
@@ -46,14 +67,15 @@ export function App() {
     placeholderData: keepPreviousData,
   });
 
-  function handleFilter() {
-    setSearchParams(params => {
-      params.set('page', '1')
-      params.set('filter', filter)
+  function handleFilter(event: FormEvent) {
+    event.preventDefault();
 
-      return params
-    })
+    setSearchParams((params) => {
+      params.set("page", "1");
+      params.set("filter", filter);
 
+      return params;
+    });
   }
 
   if (isLoading) {
@@ -69,10 +91,36 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Create new
-          </Button>
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+              <Dialog.Content className=" fixed space-y-10 p-10 right-0 top-0 bottom-0 h-screen min-w-[320px] bg-zinc-950 border-l border-zinc-900">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create Tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group videos about similar concepts.
+                  </Dialog.Description>
+                </div>
+
+                <CreateTagForm />
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+
+          {isFetching && (
+            <Loader2 className="size-4 animate-spin text-zinc-500" />
+          )}
         </div>
 
         <div className="flex items-center justify-between">
@@ -87,7 +135,7 @@ export function App() {
             </Input>
             <Button onClick={handleFilter}>
               <Filter className="size-3" />
-              Filter
+              Apply filter
             </Button>
           </div>
 
@@ -114,7 +162,7 @@ export function App() {
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
                       <span className="font-medium">{tag.title}</span>
-                      <span className="text-xs text-zinc-500">{tag.id}</span>
+                      <span className="text-xs text-zinc-500">{tag.slug}</span>
                     </div>
                   </TableCell>
                   <TableCell className="text-zinc-300">
